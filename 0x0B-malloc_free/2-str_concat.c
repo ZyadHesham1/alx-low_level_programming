80d374d94db0fd1255cd6e81e4d77ae234c48cdb
ZyadHesham1 <zyadheshamahmed@gmail.com>
2023-11-21 15:53:06 +0200

    this is a newerererererkekweaaaaaaaaaaaaaaaaaaaaaaaaaarpls2 message

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -3,8 +3,10 @@
 
 /**
  * str_concat - function that concatenates two strings.
+ *
  * @s1: first string
  * @s2: second string
+ *
  * Return: NULL on failure
  */
 
@@ -18,9 +20,10 @@ char *str_concat(char *s1, char *s2)
 	if (s2 == NULL)if (s2 == NULL)
 		s2 = "";
 
-	for (len1 = 0; s1[len1] != '\0'; len1++);
-
-	for (len2 = 0; s2[len2] != '\0'; len2++);
+	for (len1 = 0; s1[len1] != '\0'; len1++)
+		;
+	for (len2 = 0; s2[len2] != '\0'; len2++)
+		;
 
 	len = len1 + len2;
 	conStr = malloc(sizeof(char) * (len + 1));
@@ -32,11 +35,9 @@ char *str_concat(char *s1, char *s2)
 	}
 	for (i = 0; i < len1; i++)
 		conStr[i] = s1[i];
+
        	j = len2;
 	for (len2 = 0; len2 <= j; i++, len2++)
 		conStr[i] = s2[len2];
 	return (conStr);
-
 }
-       
-
